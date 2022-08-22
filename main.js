738774c552fcdd24d2eff49c899d6c932fdc1649
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-21 19:27:08 -0700

    Add eventListener mouseOver to change square color By adding addEventListener to the square variabel, we add it to all the squares on the board at the same time rather than having to add it individually. Awesome!

--- main.js
+++ main.js
@@ -2,20 +2,30 @@ function populateBoard(size){
     //Set important variables
 let board = document.querySelector(".board");
 let squares = board.querySelectorAll('div')
+// This removes the squares so that they can be set again with the new specified size
 squares.forEach((div) => div.remove());
 board.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
 board.style.gridTemplateRows= `repeat(${size}, 1fr)`;
 
-//start code
+//start of code
 let amount =size * size;
 for(i=0;i<amount;i++){
     let square= document.createElement('div');
-    square.style.backgroundColor='blue';
+// this makes it so that when the mouse hovers over a square, the color of the square changes.
+    square.addEventListener('mouseover', ()=> {
+        square.style.backgroundColor= 'black';
+    });
+    square.style.backgroundColor='white';
     board.insertAdjacentElement("beforeend",square);
     }
 }
 populateBoard(16);
 
 function changeSize(input){
-    populateBoard(input); 
+    if(input>=2 && input<=100){
+        populateBoard(input); 
+    }
+    else{
+        console.log('too many squares');
+    }
 }
