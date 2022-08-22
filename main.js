2b602dfab17c24d3a436605490d4a096cc2efa0d
carvajalejandro <alejandro.carvajal@mycase.com>
2022-08-21 21:59:53 -0700

    Add function to enable and disable color.
    I decided to make the default Not Coloring so that the user choose
    if & when they want to start drawing instead of just having to reset
    when they accidentally mouse over the sketchpad.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let color ='grey';
+let color ='black';
+let click= false;
 
 function populateBoard(size){
     //Set important variables
@@ -30,13 +31,31 @@ function changeSize(input){
 }
 // this makes it so that when the mouse hovers over a square, the color of the square changes.
 function colorSquare(){
+  if (click){
     if (color ==='random'){
-    this.style.backgroundColor= `hsl(${Math.random()* 360}, 100%, 50%)`;
-    } else{
-    this.style.backgroundColor= color;
-    }
+        this.style.backgroundColor= `hsl(${Math.random()* 360}, 100%, 50%)`;
+        } else{
+        this.style.backgroundColor= color;
+        }
+  }
 }
 function changeColor(choice){
     color= choice;
 }
 
+function resetBoard(){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.style.backgroundColor='white');
+}
+//This makes it so that you can 'click' stops coloring
+document.querySelector('body').addEventListener('click', (e)=>{
+    if(e.target.tagName!='BUTTON'){
+        click= !click;
+if(click){
+    document.querySelector('.mode').textContent='Mode: Coloring'
+} else{
+    document.querySelector('.mode').textContent='Mode: Not Coloring'
+    }
+}
+});
